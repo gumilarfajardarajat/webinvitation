a7d4a240e3d02ea3795708901d67d1202b3df18d
gumilarfajardarajat <gumilarfajardarajat@gmail.com>
2024-01-28 21:06:30 +0700

    Skeleton Fase 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(1)->create([
-            'email' => 'admin@gmail.com',
-            'name' => 'admin',
-         ]);
+        \App\Models\User::factory(10)->create();
+        //  \App\Models\User::factory(1)->create([
+        //     'email' => 'admin@gmail.com',
+        //     'name' => 'admin',
+        //  ]);
     }
 }
