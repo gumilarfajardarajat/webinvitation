4e7bf54777162cbaac71db041f185b1b36c01fab
Ryan <rhyananiki@gmail.com>
2024-01-28 09:55:19 +0700

    Views

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/master/user', function () {
+    return view('pages.masters.users.index');
+});
+
+Route::get('/master/system', function () {
+    return view('pages.masters.systems.index');
+});
+
+Route::get('/setting-template', function () {
+    return view('pages.invitations.cms.index');
 });
